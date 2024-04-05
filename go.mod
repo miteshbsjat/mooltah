56966c05b24f492907c52d7e75f855829e96ec5c
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2024-04-05 17:42:22 +0530

    Working reading yaml, json, toml files

--- go.mod
+++ go.mod
@@ -3,15 +3,31 @@ module github.com/miteshbsjat/mooltah
 go 1.19
 
 require (
-	github.com/noirbizarre/gonja v0.0.0-20200629003239-4d051fd0be61
+	github.com/BurntSushi/toml v1.3.2
+	github.com/spf13/cobra v1.8.0
+	github.com/spf13/viper v1.18.2
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
-	github.com/goph/emperror v0.17.1 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	github.com/sirupsen/logrus v1.3.0 // indirect
-	golang.org/x/crypto v0.0.0-20180904163835-0709b304e793 // indirect
-	golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,63 +1,112 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io/ioutil"
+	"log/slog"
 	"os"
+	"path/filepath"
 
-	"github.com/noirbizarre/gonja"
+	"encoding/json"
+
+	"github.com/BurntSushi/toml"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"gopkg.in/yaml.v2"
 )
 
+// mergeMaps merges two maps into one.
+func mergeMaps(m1, m2 map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+
+	for k, v := range m1 {
+		result[k] = v
+	}
+
+	for k, v := range m2 {
+		result[k] = v
+	}
+
+	return result
+}
+
+func processFiles(files []string) (map[string]interface{}, error) {
+
+	result := make(map[string]interface{})
+
+	for _, file := range files {
+		data, err := os.ReadFile(file)
+		if err != nil {
+			slog.Error("Error reading file %s: %s\n", file, err)
+			return result, err
+		}
+
+		ext := filepath.Ext(file)
+		slog.Debug("ext = " + ext)
+		switch ext {
+		case ".yaml", ".yml":
+			var yamlData map[string]interface{}
+			err = yaml.Unmarshal(data, &yamlData)
+			if err != nil {
+				slog.Error("Error unmarshalling YAML file %s: %s\n", file, err)
+				return result, err
+			}
+			result = mergeMaps(result, yamlData)
+		case ".json":
+			var jsonData map[string]interface{}
+			err = json.Unmarshal(data, &jsonData)
+			if err != nil {
+				slog.Error("Error unmarshalling JSON file %s: %s\n", file, err)
+				return result, err
+			}
+			result = mergeMaps(result, jsonData)
+		case ".toml":
+			var tomlData map[string]interface{}
+			_, err = toml.Decode(string(data), &tomlData)
+			if err != nil {
+				slog.Error("Error unmarshalling TOML file %s: %s\n", file, err)
+				return result, err
+			}
+			result = mergeMaps(result, tomlData)
+		default:
+			slog.Warn("Unsupported file format for file %s\n", file)
+		}
+	}
+
+	return result, nil
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "mooltah",
+	Short: "A command-line tool for processing YAML, JSON, and TOML files",
+	Run:   runMooltah,
+}
+
+func runMooltah(cmd *cobra.Command, args []string) {
+	files := viper.GetStringSlice("files")
+
+	var result map[string]interface{}
+
+	result, err := processFiles(files)
+	if err != nil {
+		slog.Error("Failed %v", err)
+		return
+	}
+
+	fmt.Println(result)
+}
+
 func main() {
-	// Define command-line flags
-	templateFile := flag.String("template", "", "Path to the Jinja2 template file")
-	dataFile := flag.String("data", "", "Path to the YAML data file")
-	flag.Parse()
+	cobra.OnInitialize(initConfig)
 
-	// Check if both template and data files are provided
-	if *templateFile == "" || *dataFile == "" {
-		fmt.Println("Both template and data files are required")
-		flag.PrintDefaults()
+	rootCmd.Flags().StringSliceP("files", "v", []string{}, "List of files to process")
+	viper.BindPFlag("files", rootCmd.Flags().Lookup("files"))
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
+}
 
-	// Read template file
-	// templateContent, err := ioutil.ReadFile(*templateFile)
-	templateContent, err := ioutil.ReadFile(*templateFile)
-	if err != nil {
-		fmt.Printf("Error reading template file: %s\n", err)
-		os.Exit(1)
-	}
-
-	// Read data file
-	dataContent, err := ioutil.ReadFile(*dataFile)
-	if err != nil {
-		fmt.Printf("Error reading data file: %s\n", err)
-		os.Exit(1)
-	}
-
-	// Parse YAML data
-	var data map[string]interface{}
-	if err := yaml.Unmarshal(dataContent, &data); err != nil {
-		fmt.Printf("Error parsing YAML data: %s\n", err)
-		os.Exit(1)
-	}
-
-	// Compile the template first (i. e. creating the AST)
-	tpl, err := gonja.FromString(string(templateContent))
-	if err != nil {
-		panic(err)
-	}
-	// Now you can render the template with the given
-	// gonja.Context how often you want to.
-	// out, err := tpl.Execute(gonja.Context{"name": "axel"})
-	out, err := tpl.Execute(data)
-	if err != nil {
-		panic(err)
-	}
-
-	// Print the rendered output
-	fmt.Println(out)
+func initConfig() {
+	viper.AutomaticEnv()
 }
